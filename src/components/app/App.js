d9e9a4f08aa71227540d9a30d662af60ce027f76
Vinor <vinorval@yandex.ru>
2023-12-09 12:44:56 +0300

    create 404 page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Router, Route } from 'react-router';
+
+import NotFoundPage from '../../page/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route path='*' element={<NotFoundPage />} />
+      </Router>
     </div>
   );
 }
