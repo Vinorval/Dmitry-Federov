5f42cb344ad8a26cedf8de0e7b0ff5aefcf0e113
Vinor <vinorval@yandex.ru>
2023-12-10 15:44:56 +0300

    add style for header and navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,12 +3,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-    const arrLinks = [ "Мастер", "Каталог", "LIVE", "Контакты" ];
+    const arrLinks = [ { name: "Мастер", to: "/master" }, { name: "Каталог", to: "/catalog" }, { name: "LIVE", to: "/live" }, { name: "Контакты", to: "/contacts" } ];
     const returnLink = (link, key) => (
-        <NavLink className={({ isActive }) => [
+        <NavLink to={link.to} className={({ isActive }) => [
             Styles.navigation__text,
             isActive ? Styles.navigation__text_type_active : null
-        ].filter(Boolean).join(" ")} key={key}>{link}</NavLink>
+        ].filter(Boolean).join(" ")} key={key}>{link.name}</NavLink>
     )
 
     return (
