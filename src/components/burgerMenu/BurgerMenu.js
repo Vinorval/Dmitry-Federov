4702108c5f9b62bdfb99e84b3ec17957107ea70b
Vinor <vinorval@yandex.ru>
2023-12-11 11:44:56 +0300

    add styles for burger-menu

--- src/components/burgerMenu/BurgerMenu.js
+++ src/components/burgerMenu/BurgerMenu.js
@@ -3,14 +3,17 @@ import React from "react";
 
 import Navigation from "../navigation/Navigation";
 
+import Close from "../../images/icons/close.svg";
+import WhatsApp from "../../images/icons/WhatsApp.svg";
+
 const BurgerMenu = ({ close }) => {
     return (
         <div className={Styles.menu} >
-            <button onClick={close} ><img /></button>
+            <button onClick={close} className={Styles.menu__closeButton} ><img alt="кнокпа закрытия меню" src={Close} className={Styles.menu__iconClose} /></button>
             <Navigation burger={true} />
-            <div>
-                <p className={Styles.menu__phone} >+7 (999) 808 65 48</p>
-                <img />
+            <div className={Styles.phone} >
+                <img alt="иконка whatsapp" src={WhatsApp} className={Styles.phone__social} />
+                <p className={Styles.phone__number} >+7 (999) 808 65 48</p>
             </div>
         </div>
     )
