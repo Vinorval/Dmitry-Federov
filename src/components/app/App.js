348f920f02bc7bef0e78097e2dca621409b9d95f
Vinor <vinorval@yandex.ru>
2023-12-11 13:09:56 +0300

    create page about master

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,14 +3,15 @@ import { Routes, Route } from 'react-router';
 import AppHeader from '../appHeader.js/AppHeader';
 
 import NotFoundPage from '../../page/NotFoundPage';
+import MasterPage from '../../page/MasterPage';
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
       <Routes>
-        <Route path="/master" element={<NotFoundPage />} />
-        <Route path='/' element={<NotFoundPage />} />
+        <Route path="/master" element={<MasterPage />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/page/MasterPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MasterPage = () => {
+    return (
+        <></>
+    )
+};
+
+export default MasterPage;
